781f5039cc011eff2ccf3f6fde5fc5785093d93b
Thomas PRAMPART <thomas.prampart@inria.fr>
2020-08-05 18:45:27 +0200

    Resolve "Update: cmake version for windows"

--- install_dependencies.cmd
+++ install_dependencies.cmd
@@ -4,7 +4,7 @@ setlocal enableextensions
 
 REM The dependencies are hosted by Inria
 set PROXYPASS=anon:anon
-set URL=http://openvibe.inria.fr/dependencies/win32/2.2.0/
+set URL=http://openvibe.inria.fr/dependencies/win32/3.0.0/
 
 set base_dir=%~dp0
 set dependencies_prefix=%base_dir%\dependencies
